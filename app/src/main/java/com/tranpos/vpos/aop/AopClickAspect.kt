aaac0d2e34f799f28298d6810321386c8c312f03
e <mjmmast2012@163.com>
2021-01-15 13:47:38 +0800

    aop

---
+++ app/src/main/java/com/tranpos/vpos/aop/AopClickAspect.kt
@@ -0,0 +1,43 @@
+package com.transpos.market.aop
+
+import com.tranpos.vpos.aop.AopClickUtil
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
+import org.aspectj.lang.reflect.MethodSignature
+
+/**
+ *CREATED BY AM
+ *ON 2020/8/14
+ *DESCRIPTION:点击事件单点处理
+ **/
+@Aspect
+class AopClickAspect {
+    /**
+     * 定义切点，标记切点为所有被@AopOnclick注解的方法
+     */
+    @Pointcut("execution(@com.transpos.market.aop.AopOnclick * *(..))")
+    fun methodAnnotated() {
+    }
+
+    /**
+     * 定义一个切面方法，包裹切点方法
+     */
+    @Around("methodAnnotated()")
+    @Throws(Throwable::class)
+    fun aroundJoinPoint(joinPoint: ProceedingJoinPoint) {
+        // 取出方法的注解
+        val methodSignature = joinPoint.signature as MethodSignature
+        val method = methodSignature.method
+        if (!method.isAnnotationPresent(AopOnclick::class.java)) {
+            return
+        }
+        val aopOnclick = method.getAnnotation(AopOnclick::class.java)
+        // 判断是否快速点击
+        if (!AopClickUtil.isFastDoubleClick(aopOnclick.value)) {
+            // 不是快速点击，执行原方法
+            joinPoint.proceed()
+        }
+    }
+}
